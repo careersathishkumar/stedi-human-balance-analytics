cd2db80aa2bdd9ca52a1002902d8651defa7c767
careersathishkumar <career.sathishkumar@gmail.com>
2026-06-03 09:56:32 +0530

    Update accelerometer_landing_to_trusted.py

--- accelerometer_landing_to_trusted.py
+++ accelerometer_landing_to_trusted.py
@@ -4,6 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -12,20 +13,15 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-
+# Read from S3
 accelerometer_df = spark.read.json(
     "s3://stedi-project-sathish/accelerometer/landing/"
 )
-
-
 customer_df = spark.read.json(
     "s3://stedi-project-sathish/customer/trusted/"
 )
 
-
-accelerometer_df.printSchema()
-customer_df.printSchema()
-
+# Join on email
 result_df = accelerometer_df.join(
     customer_df,
     accelerometer_df["user"] == customer_df["email"],
@@ -38,8 +34,25 @@ result_df = accelerometer_df.join(
     accelerometer_df["z"]
 )
 
-result_df.write.mode("overwrite").json(
-    "s3://stedi-project-sathish/accelerometer/trusted/"
+# Convert to DynamicFrame
+dynamic_frame = DynamicFrame.fromDF(
+    result_df,
+    glueContext,
+    "dynamic_frame"
 )
+
+# Write and update Glue Catalog
+sink = glueContext.getSink(
+    connection_type="s3",
+    path="s3://stedi-project-sathish/accelerometer/trusted/",
+    enableUpdateCatalog=True,
+    updateBehavior="UPDATE_IN_DATABASE"
+)
+sink.setFormat("json")
+sink.setCatalogInfo(
+    catalogDatabase="stedi",
+    catalogTableName="accelerometer_trusted"
+)
+sink.writeFrame(dynamic_frame)
 
 job.commit()
