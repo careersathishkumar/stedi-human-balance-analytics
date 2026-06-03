9c310b4d74a5ecc681797cec19637b0e3bd4b457
careersathishkumar <career.sathishkumar@gmail.com>
2026-06-03 10:00:41 +0530

    Update machine_learning_curated.py

--- machine_learning_curated.py
+++ machine_learning_curated.py
@@ -4,6 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -16,8 +17,6 @@ job.init(args['JOB_NAME'], args)
 step_trainer_df = spark.read.json(
     "s3://stedi-project-sathish/step_trainer/trusted/"
 )
-
-
 accelerometer_df = spark.read.json(
     "s3://stedi-project-sathish/accelerometer/trusted/"
 )
@@ -38,8 +37,24 @@ result_df = step_trainer_df.join(
     accelerometer_df["z"]
 )
 
-result_df.write.mode("overwrite").json(
-    "s3://stedi-project-sathish/machine_learning/curated/"
+
+dynamic_frame = DynamicFrame.fromDF(
+    result_df,
+    glueContext,
+    "dynamic_frame"
 )
+
+sink = glueContext.getSink(
+    connection_type="s3",
+    path="s3://stedi-project-sathish/machine_learning/curated/",
+    enableUpdateCatalog=True,
+    updateBehavior="UPDATE_IN_DATABASE"
+)
+sink.setFormat("json")
+sink.setCatalogInfo(
+    catalogDatabase="stedi",
+    catalogTableName="machine_learning_curated"
+)
+sink.writeFrame(dynamic_frame)
 
 job.commit()
